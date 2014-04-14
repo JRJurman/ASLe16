3893fd344ca1be34daeb67a8a4e48cef7185e616
JRJurman <j.r.jurman@gmail.com>
2014-04-14 00:03:30 -0400

    fixed the valid checking, and made extra 0s only if invalid

--- interactive_builder.py
+++ interactive_builder.py
@@ -1,11 +1,11 @@
-import curses
+import curses, re
 from PyDecipher import PyDecipher
 from asl_encoding import asl_encoding
 
 pd = PyDecipher(asl_encoding)
 
 stdscr = curses.initscr()
-ui = curses.newwin(3, 17, 1, 0)
+ui = curses.newwin(3, 36, 1, 0)
 res = curses.newwin(40, 40, 4, 0)
 
 curses.noecho()
@@ -25,22 +25,31 @@ while 1:
         break  # Exit the while()
     elif c == 127:
         uiString = uiString[:len(uiString)-1]
-    else:
+    elif c == ord('0') or c == ord('1'):
         uiString += chr(c)
-
-    # set curses color
-    if pd.isValid(uiString):
-        valColor = 42
     else:
-        valColor = 10
+        pass
+
+
+    ui.clear();
+    res.clear();
 
     # print result string
-    ui.clear();
-    ui.addstr(0, 0, "00000000 00000000", curses.color_pair(60))
-    ui.addstr(0, 0, uiString, curses.color_pair(valColor))
+    splitUI = " ".join( i for i in re.findall("([01]{,8})",uiString) if i != "")
+
+    # set curses color
+    if pd.isValid(splitUI):
+        valColor = 42
+
+    else:
+        valColor = 10
+        # print binary outline ONLY if our string isn't already complete
+        ui.addstr(0, 0, "00000000 00000000 "*((len(uiString)//16)+1), curses.color_pair(60))
+
+
+    ui.addstr(0, 0, splitUI, curses.color_pair(valColor))
     ui.refresh()
 
-    res.clear();
-    res.addstr(0, 0, pd.decipher(uiString))
+    res.addstr(0, 0, pd.decipher(splitUI))
     res.refresh()
 
