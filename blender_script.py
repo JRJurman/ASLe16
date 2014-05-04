9e9c9a0647d57de2a95ef3c6c3bbc10c671bfb97
Ethan H. Jurman <ehj2229@rit.edu>
2014-05-04 17:54:22 -0400

    b. builds and tmp directory, modified the batch file accordingly

--- blender_script.py
+++ blender_script.py
@@ -13,3 +13,22 @@ bpy.ops.poselib.apply_pose(pose_index=2)
 bpy.data.armatures["Armature"]["wrist_roll_R"] = -3.0
 bpy.data.armatures["Armature"]["wrist_yaw_R"] = -1.0
 
+##################################################
+
+# thumb L
+rig.pose.bone_groups.active_index = 0
+bpy.ops.pose.group_select()
+
+# change poses - finger pose - hand close
+rig.pose_library = bpy.data.actions['FingerPoses']
+bpy.ops.poselib.apply_pose(pose_index=2)
+
+##################################################
+
+# thumb R
+rig.pose.bone_groups.active_index = 6 # different hand is 5 above
+bpy.ops.pose.group_select()
+
+# change poses = thumb poses - to web 1
+rig.pose_library = bpy.data.actions['ThumbPoses']
+bpy.ops.poselib.apply_pose(pose_index=0)
