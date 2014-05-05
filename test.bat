9d23b18c65afbfa7d105932dbe8fdd5e397a60fc
Jesse Jurman <j.r.jurman@gmail.com>
2014-05-05 01:25:44 -0400

    added convert command

--- test.bat
+++ test.bat
@@ -1,1 +1,2 @@
-"blenderBuilds\blender-2.70a-windows32\blender.exe" -b "ASL_Model.blend" -P "blender_script.py" -y -o "tmp\asl" -F PNG -x 1 -f 1
+"C:\Program Files\Blender Foundation\Blender\blender.exe" -b "ASL_Model.blend" -P "blender_script.py" -y -o "tmp\asl" -F PNG -x 1 -f 1
+convert tmp\asl0001.png tmp\asl0001.gif
