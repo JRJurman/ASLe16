6c8499a9227676995f890fadd3c1e510de038256
Ethan H. Jurman <ehj2229@rit.edu>
2014-05-06 15:01:12 -0400

    updated blend file and script

--- blender_script.py
+++ blender_script.py
@@ -12,8 +12,8 @@ bpy.ops.pose.select_all(action='DESELECT') # removes all selections
 # example code
 # select hands
 for i in range(10):
-    rig.pose.bone_groups.active_index = i
-    bpy.ops.pose.group_select()
+	rig.pose.bone_groups.active_index = i
+	bpy.ops.pose.group_select()
 bpy.ops.poselib.apply_pose(pose_index=2)
 # wrist turn
 armature["Armature"]["wrist_roll_R"] = -3.0
@@ -34,36 +34,38 @@ def leftHandLocation( position ):
 	rig.pose.bone_groups.active_index = 11
 	bpy.ops.pose.group_select()
 	rig.pose_library = bpy.data.actions['Locations']
+	positions = ['target_chest_L', 'target_forearm_L', 'target_part_location_L', 'target_upper_arm_L']
+	for i in positions:
+		armature[i] = 0
 	if position in rig.pose_library.pose_markers.keys():
 		bpy.ops.poselib.apply_pose(pose_index=rig.pose_library.pose_markers.keys().index( position ))
 	else:
 		if position == "waist-center":
 			armature["target_chest_L"] = 1
-			armature["part_location_L"] = 0
+			armature["target_part_location_L"] = 0
 		elif position == "forearm":
 			armature["target_forearm_L"] = 1
-			armature["part_location_L"] = 0
+			armature["target_part_location_L"] = 0
 		elif position == "back-of-wrist":
 			armature["target_forearm_L"] = 1
-			armature["part_location_L"] = 1
+			armature["target_part_location_L"] = 1
 		elif position == "inside-of-wrist": # TOOD : get wrist rot / loc
 			armature["target_forearm_L"] = 1
-			armature["part_location_L"] = 1
-		elif position == "hand": # TODO : get hand
-			armature["target_forearm_L"] = 1
-			armature["part_location_L"] = 1
+			armature["target_part_location_L"] = 1
+		elif position == "hand":
+			armature["target_hand_L"] = 1
 		elif position == "torso-center":
 			armature["target_chest_L"] = 1
-			armature["part_location_L"] = 0.5
+			armature["target_part_location_L"] = 0.5
 		elif position == "upper-arm":
 			armature["target_upper_arm_L"] = 1
-			armature["part_location_L"] = 0.5
+			armature["target_part_location_L"] = 0.5
 		elif position == "elbow":
 			armature["target_upper_arm_L"] = 1
-			armature["part_location_L"] = 1
+			armature["target_part_location_L"] = 1
 		elif position == "manubrium":
 			armature["target_chest_L"] = 1
-			armature["part_location_L"] = 1
+			armature["target_part_location_L"] = 1
 		else:
 			print("INVALID VALUE func leftHandLocation")
 		
@@ -71,41 +73,42 @@ def rightHandLocation( position ):
 	rig.pose.bone_groups.active_index = 10
 	bpy.ops.pose.group_select()
 	rig.pose_library = bpy.data.actions['Locations']
+	positions = ['target_chest_R', 'target_forearm_R', 'target_part_location_R', 'target_upper_arm_R']
+	for i in positions:
+		armature[i] = 0
 	if position in rig.pose_library.pose_markers.keys():
 		bpy.ops.poselib.apply_pose(pose_index=rig.pose_library.pose_markers.keys().index( position ))
 	else:
 		if position == "waist-center":
 			armature["target_chest_R"] = 1
-			armature["part_location_R"] = 0
+			armature["target_part_location_R"] = 0
 		elif position == "forearm":
 			armature["target_forearm_R"] = 1
-			armature["part_location_R"] = 0
+			armature["target_part_location_R"] = 0
 		elif position == "back-of-wrist":
 			armature["target_forearm_R"] = 1
-			armature["part_location_R"] = 1
+			armature["target_part_location_R"] = 1
 		elif position == "inside-of-wrist": # TOOD : get wrist rot / loc
 			armature["target_forearm_R"] = 1
-			armature["part_location_R"] = 1
-		elif position == "hand": # TODO : get hand
-			armature["target_forearm_R"] = 1
-			armature["part_location_R"] = 1
+			armature["target_part_location_R"] = 1
+		elif position == "hand":
+			armature["target_hand_R"] = 1
 		elif position == "torso-center":
 			armature["target_chest_R"] = 1
-			armature["part_location_R"] = 0.5
+			armature["target_part_location_R"] = 0.5
 		elif position == "upper-arm":
 			armature["target_upper_arm_R"] = 1
-			armature["part_location_R"] = 0.5
+			armature["target_part_location_R"] = 0.5
 		elif position == "elbow":
 			armature["target_upper_arm_R"] = 1
-			armature["part_location_R"] = 1
+			armature["target_part_location_R"] = 1
 		elif position == "manubrium":
 			armature["target_chest_R"] = 1
-			armature["part_location_R"] = 1
+			armature["target_part_location_R"] = 1
 		else:
 			print("INVALID VALUE func rightHandLocation")
 		
 def leftHandShape( position ):
-	print( position )
 	for i in range(0,5):
 		rig.pose.bone_groups.active_index = i
 		bpy.ops.pose.group_select()
@@ -113,10 +116,9 @@ def leftHandShape( position ):
 	if position in rig.pose_library.pose_markers.keys():
 		bpy.ops.poselib.apply_pose(pose_index=rig.pose_library.pose_markers.keys().index( position ))
 	else:
-        	print("INVALID VALUE func leftHandShape: {}".format(position))
+		print("INVALID VALUE func leftHandShape: {}".format(position))
 
 def rightHandShape( position ):
-	print( position )
 	for i in range(5,10):
 		rig.pose.bone_groups.active_index = i
 		bpy.ops.pose.group_select()
@@ -204,26 +206,17 @@ def rightWrist( modifierName, value ):
 			mod = -2
 		armature["wrist_yaw_R"] = mod
 		
-def leftTargetFinger( value ):
-	if value != "none":
-		pTargetName = "L_" + value + "_target"
-		armature[pTargetName] = 1
-	else:
-		for f in ["thumb", "index", "middle", "ring", "pinky"]:
-			pTargetName = "L_" + f + "_target"
-			armature[pTargetName] = 0
+def leftTargetModifiers( value ):
+	for f in ["thumb", "index", "middle", "ring", "pinky"]:
+		pTargetName = "L_" + f + "_target"
+		armature[pTargetName] = 1 if value == f else 0
 		
-def rightTargetFinger( value ):
-	if value != "none":
-		pTargetName = "R_" + value + "_target"
-		armature[pTargetName] = 1
-	else:
-		for f in ["thumb", "index", "middle", "ring", "pinky"]:
-			pTargetName = "R_" + f + "_target"
-			armature[pTargetName] = 0
+def rightTargetModifiers( value ):
+	for f in ["thumb", "index", "middle", "ring", "pinky"]:
+		pTargetName = "R_" + f + "_target"
+		armature[pTargetName] = 1 if value == f else 0
 
 def before():
-	print("de-select")
 	bpy.ops.pose.select_all(action='DESELECT') # removes all selections
 	
 pd.setBeforeRegister(before)
@@ -235,8 +228,8 @@ pd.register(leftThumb, "LeftThumb", "Bend")
 pd.register(rightThumb, "RightThumb", "Bend")
 pd.register(leftWrist, "LeftWrist")
 pd.register(rightWrist, "RightWrist")
-pd.register(leftTargetFinger, "LeftTargetFinger", "Finger")
-pd.register(rightTargetFinger, "RightTargetFinger", "Finger")
+pd.register(leftTargetModifiers, "LeftTargetModifiers", "Finger")
+pd.register(rightTargetModifiers, "RightTargetModifiers", "Finger")
 
 bpy.context.scene.render.image_settings.file_format = 'BMP'
 frame = bpy.data.scenes["bone+mesh"].frame_current
@@ -244,15 +237,13 @@ strFrame = "0"*(4-len(str(frame)))+str(frame)
 bpy.data.scenes["bone+mesh"].render.filepath = "./tmp/asl" + strFrame
 
 while True:
-    #pd.callFunctions(open("tmp/encode.txt").read())
-    commIn = input(":: ")
-    print("Got the following code:: {}".format(commIn))
-    pd.callFunctions(commIn)
-    bpy.ops.render.render( write_still=True )
-    # check if lock was created
-    if os.path.exists("tmp/.lock"):
-        print("FOUND LOCK")
-        #removing lock
-        os.remove("tmp/.lock")
-    else:
-        print("LOCK NOT FOUND")
+	#pd.callFunctions(open("tmp/encode.txt").read())
+	commIn = input()
+	pd.callFunctions(commIn)
+	bpy.ops.render.render( write_still=True )
+	# check if lock was created
+	if os.path.exists("tmp/.lock"):
+		#removing lock
+		os.remove("tmp/.lock")
+	else:
+		print("LOCK NOT FOUND")
